ac644e7014c50066f7ab41350c7b257025f00e55
kenielnunes <kenielalves3@gmail.com>
2026-02-02 16:57:30 -0300

    refactor:  remove unnecessary line breaks

--- frontend/src/components/ui/table.tsx
+++ frontend/src/components/ui/table.tsx
@@ -1,15 +1,11 @@
 "use client"
 
 import * as React from "react"
-
 import { cn } from "@/lib/utils"
 
 function Table({ className, ...props }: React.ComponentProps<"table">) {
   return (
-    <div
-      data-slot="table-container"
-      className="relative w-full overflow-x-auto"
-    >
+    <div data-slot="table-container" className="relative w-full overflow-x-auto">
       <table
         data-slot="table"
         className={cn("w-full caption-bottom text-sm", className)}
@@ -91,10 +87,7 @@ function TableCell({ className, ...props }: React.ComponentProps<"td">) {
   )
 }
 
-function TableCaption({
-  className,
-  ...props
-}: React.ComponentProps<"caption">) {
+function TableCaption({ className, ...props }: React.ComponentProps<"caption">) {
   return (
     <caption
       data-slot="table-caption"
